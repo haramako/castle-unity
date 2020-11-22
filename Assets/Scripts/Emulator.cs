d87b64bf8e0a0a2ab473fea04f4d703241b35d96
HARADA Makoto <haramako@gmail.com>
2020-11-22 20:38:26 +0900

    Emulatorクラスを分離

--- Assets/Scripts/Emulator.cs
+++ Assets/Scripts/Emulator.cs
@@ -1,21 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using System;
 using System.Text;
 using System.Runtime.InteropServices;
-using UnityEngine.UI;
 using UnityEngine.Profiling;
-using System.IO;
-using UnityEngine.Networking;
-using UnityEngine.EventSystems;
 
 #pragma warning disable IDE1006
 
-public class TestScene : MonoBehaviour
+public class Emulator : MonoSingleton<Emulator>
 {
-    static Texture2D tex_;
-    public RawImage RawImage;
+    int[] KeyInputs = new int[4 * 2 * 1 * 14];
+
+    public Texture2D ScreenTexture { get; private set; }
 
     [AOT.MonoPInvokeCallback(typeof(LibRetro.retro_environment_t))]
     public static unsafe int environment_callback(LibRetro.Environment cmd, void* data)
@@ -87,8 +85,8 @@ public class TestScene : MonoBehaviour
     public static unsafe void video_refresh_callback(UInt32* data, UInt32 width, UInt32 height, UInt64 pitch)
     {
         //Debug.Log($"video callback {width} {height} {pitch}");
-        tex_.LoadRawTextureData(new IntPtr((void*)data), (int)(width * height * sizeof(UInt32)));
-        tex_.Apply();
+        Instance.ScreenTexture.LoadRawTextureData(new IntPtr((void*)data), (int)(width * height * sizeof(UInt32)));
+        Instance.ScreenTexture.Apply();
     }
 
     [AOT.MonoPInvokeCallback(typeof(LibRetro.retro_audio_sample_batch_t))]
@@ -107,7 +105,7 @@ public class TestScene : MonoBehaviour
     public static Int16 input_state_callback(UInt32 port, UInt32 device, UInt32 index, UInt32 id)
     {
         //Debug.Log($"input state callback {port} {device} {index} {id}");
-        return (Int16)KeyInputs[keyIndex((int)port, (int)device, (int)index, (int)id)];
+        return (Int16)Instance.KeyInputs[keyIndex((int)port, (int)device, (int)index, (int)id)];
     }
 
     [AOT.MonoPInvokeCallback(typeof(LibRetro.retro_log_printf_t))]
@@ -116,12 +114,10 @@ public class TestScene : MonoBehaviour
         Debug.Log($"LOG: {fmt}");
     }
 
-
-    unsafe void setupRetro()
+    public unsafe void Setup()
     {
-        tex_ = new Texture2D(256, 240, TextureFormat.BGRA32, false);
+        ScreenTexture = new Texture2D(256, 240, TextureFormat.BGRA32, false);
         //tex_ = new Texture2D(256, 240, TextureFormat.R8, false);
-        RawImage.texture = tex_;
 
         LibRetro.retro_set_environment(environment_callback);
         Debug.Log("set_environment");
@@ -148,168 +144,32 @@ public class TestScene : MonoBehaviour
         }
     }
 
-    static readonly Dictionary<KeyCode, int> KeyMap = new Dictionary<KeyCode, int> {
-        {KeyCode.UpArrow, 4 },
-        {KeyCode.DownArrow, 5 },
-        {KeyCode.LeftArrow, 6 },
-        {KeyCode.RightArrow, 7 },
-        {KeyCode.X, 8 },
-        {KeyCode.Z, 0 },
-        {KeyCode.Q, 2 },
-        {KeyCode.W, 3 },
-    };
-
-    static int[] KeyInputs = new int[4*2*1*14];
-
-    static int keyIndex(int port, int device, int index, int id)
+    public void RunOneFrame()
     {
-        return ((port * 2  + device ) * 1 + index) * 14 + id;
-    }
+        Profiler.BeginSample("retro_run");
+        LibRetro.retro_run();
+        Profiler.EndSample();
+        //Debug.Log("retru_run");
 
-    void updateInput()
-    {
-        for( int i=0; i<KeyInputs.Length; i++)
+        // Clear inputs.
+        for (int i = 0; i < KeyInputs.Length; i++)
         {
             KeyInputs[i] = 0;
         }
 
-        foreach ( var kv in KeyMap)
-        {
-            if( Input.GetKey(kv.Key))
-            {
-                KeyInputs[keyIndex(0, 1, 0, kv.Value)] = 1;
-            }
-        }
-
-        updateTouch();
     }
 
-
-    void updateTouch()
+    static int keyIndex(int port, int device, int index, int id)
     {
-        var canvas = FindObjectOfType<Canvas>();
-        foreach (var t in Input.touches)
-        {
-            var pos = t.position;
-            Debug.LogError($"touch {t.fingerId}");
-            touchButton(pos);
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            touchButton(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        }
-
+        return ((port * 2 + device) * 1 + index) * 14 + id;
     }
 
-    Canvas canvas_;
-
-    void touchButton(Vector2 pos)
+    public void UpdateKey(int port, int device, int index, int id, int val)
     {
-        //Debug.LogError(pos);
-        //var ray = canvas.worldCamera.ScreenPointToRay(pos);
-        var raycaster = canvas_.GetComponent<GraphicRaycaster>();
-        foreach (var touchArea in touchAreas_)
-        {
-            var rt = touchArea.GetComponent<RectTransform>();
-            var contains = RectTransformUtility.RectangleContainsScreenPoint(rt, pos);
-            if (contains)
-            {
-                if (touchArea.ButtonId == -1)
-                {
-                    if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, pos, null, out var localPoint))
-                    {
-                        if (localPoint.x < -30)
-                        {
-                            KeyInputs[keyIndex(0, 1, 0, 6)] = 1;
-                        }
-                        else if(localPoint.x > 30 )
-                        {
-                            KeyInputs[keyIndex(0, 1, 0, 7)] = 1;
-                        }
-                        if (localPoint.y < -30)
-                        {
-                            KeyInputs[keyIndex(0, 1, 0, 5)] = 1;
-                        }
-                        else if (localPoint.y > 30)
-                        {
-                            KeyInputs[keyIndex(0, 1, 0, 4)] = 1;
-                        }
-                    }
-                }
-                else
-                {
-                    KeyInputs[keyIndex(0, 1, 0, touchArea.ButtonId)] = 1;
-                }
-            }
-        }
+        KeyInputs[keyIndex(port, device, index, id)] = val;
     }
 
-    // Start is called before the first frame update
-    IEnumerator Start()
-    {
-        Application.targetFrameRate = 60;
-
-        setupUi();
-        yield return setupFiles();
-        setupRetro();
-
-        load();
-
-        while (true)
-        {
-            if (Input.GetKey(KeyCode.S))
-            {
-                save();
-            }
-            else if (Input.GetKey(KeyCode.L))
-            {
-                load();
-            }
-            else if (Input.GetKey(KeyCode.R))
-            {
-                reset();
-            }
-
-            updateInput();
-            Profiler.BeginSample("retro_run");
-            LibRetro.retro_run();
-            Profiler.EndSample();
-            Debug.Log("run");
-            yield return null;
-        }
-
-    }
-
-#pragma warning disable CS0618 // WWWを使う
-    IEnumerator download(string url, string savePath)
-    {
-        var request = new WWW(url);
-        yield return request;
-        if( request.error != null)
-        {
-            throw new Exception(request.error);
-        }
-        File.WriteAllBytes(savePath, request.bytes);
-        Debug.Log($"Download {url}");
-    }
-#pragma warning restore CS0618
-
-    IEnumerator setupFiles()
-    {
-        yield return download(Application.streamingAssetsPath + "/castle.nes", Path.Combine(Application.persistentDataPath, "castle.nes"));
-        yield return download(Application.streamingAssetsPath + "/custom.pal", Path.Combine(Application.persistentDataPath, "custom.pal"));
-    }
-
-    TouchArea[] touchAreas_;
-
-    void setupUi()
-    {
-        canvas_ = FindObjectOfType<Canvas>();
-        touchAreas_ = FindObjectsOfType<TouchArea>();
-    }
-
-    void reset()
+    public void ResetMachine()
     {
         LibRetro.retro_reset();
     }
@@ -319,11 +179,11 @@ public class TestScene : MonoBehaviour
         return Path.Combine(Application.persistentDataPath, "save.dat");
     }
 
-    void save()
+    public void SaveState()
     {
         var len = LibRetro.retro_serialize_size();
         var buf = new byte[len];
-        if( !LibRetro.retro_serialize(buf, len))
+        if (!LibRetro.retro_serialize(buf, len))
         {
             throw new Exception("Serialize failed");
         }
@@ -331,38 +191,22 @@ public class TestScene : MonoBehaviour
         Debug.Log($"save {len}");
     }
 
-    void load()
+    public void LoadState()
     {
-        if( !File.Exists(savePath()))
+        if (!File.Exists(savePath()))
         {
             return;
         }
 
         var len = LibRetro.retro_serialize_size();
         var buf = File.ReadAllBytes(savePath());
-        if ( !LibRetro.retro_unserialize(buf, len))
+        if (!LibRetro.retro_unserialize(buf, len))
         {
             throw new Exception("Unserialize failed");
         }
         Debug.Log($"load {len}");
     }
 
-    void OnApplicationQuit()
-    {
-        save();
-    }
-
-    private void OnApplicationPause(bool pause)
-    {
-        if( pause)
-        {
-            save();
-        }
-    }
-
-    public void OnResetButtonClick()
-    {
-        reset();
-    }
 
 }
+
---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+using UnityEngine.Profiling;
+using System.IO;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
+
+public class GameScene : MonoSingleton<GameScene>
+{
+    public RawImage RawImage;
+    public Emulator Emulator;
+
+    Canvas canvas_;
+
+    static readonly Dictionary<KeyCode, int> KeyMap = new Dictionary<KeyCode, int> {
+        {KeyCode.UpArrow, 4 },
+        {KeyCode.DownArrow, 5 },
+        {KeyCode.LeftArrow, 6 },
+        {KeyCode.RightArrow, 7 },
+        {KeyCode.X, 8 },
+        {KeyCode.Z, 0 },
+        {KeyCode.Q, 2 },
+        {KeyCode.W, 3 },
+    };
+
+    void updateInput()
+    {
+        updateJoypad();
+        updateTouchAndMouse();
+    }
+
+    void updateJoypad()
+    {
+        foreach (var kv in KeyMap)
+        {
+            if (Input.GetKey(kv.Key))
+            {
+                Emulator.UpdateKey(0, 1, 0, kv.Value, 1);
+            }
+        }
+    }
+
+    void updateTouchAndMouse()
+    {
+        // Touch
+        foreach (var t in Input.touches)
+        {
+            var pos = t.position;
+            Debug.LogError($"touch {t.fingerId}");
+            touchButton(pos);
+        }
+
+        // Mouse
+        if (Input.GetMouseButton(0))
+        {
+            touchButton(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        }
+
+    }
+
+    void touchButton(Vector2 pos)
+    {
+        //Debug.LogError(pos);
+        //var ray = canvas.worldCamera.ScreenPointToRay(pos);
+        var raycaster = canvas_.GetComponent<GraphicRaycaster>();
+        foreach (var touchArea in touchAreas_)
+        {
+            var rt = touchArea.GetComponent<RectTransform>();
+            var contains = RectTransformUtility.RectangleContainsScreenPoint(rt, pos);
+            if (contains)
+            {
+                if (touchArea.ButtonId == -1)
+                {
+                    if (RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, pos, null, out var localPoint))
+                    {
+                        if (localPoint.x < -30)
+                        {
+                            Emulator.UpdateKey(0, 1, 0, 6, 1);
+                        }
+                        else if (localPoint.x > 30)
+                        {
+                            Emulator.UpdateKey(0, 1, 0, 7, 1);
+                        }
+                        if (localPoint.y < -30)
+                        {
+                            Emulator.UpdateKey(0, 1, 0, 5, 1);
+                        }
+                        else if (localPoint.y > 30)
+                        {
+                            Emulator.UpdateKey(0, 1, 0, 4, 1);
+                        }
+                    }
+                }
+                else
+                {
+                    Emulator.UpdateKey(0, 1, 0, touchArea.ButtonId,1);
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        Application.targetFrameRate = 60;
+
+        setupUi();
+        yield return setupFiles();
+        Emulator.Setup();
+
+        RawImage.texture = Emulator.ScreenTexture;
+
+        Emulator.LoadState();
+
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                Emulator.SaveState();
+            }
+            else if (Input.GetKey(KeyCode.L))
+            {
+                Emulator.LoadState();
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                Emulator.ResetMachine();
+            }
+
+            updateInput();
+            Emulator.RunOneFrame();
+            yield return null;
+        }
+
+    }
+
+#pragma warning disable CS0618 // WWWを使う
+    IEnumerator download(string url, string savePath)
+    {
+        var request = new WWW(url);
+        yield return request;
+        if( request.error != null)
+        {
+            throw new Exception(request.error);
+        }
+        File.WriteAllBytes(savePath, request.bytes);
+        Debug.Log($"Download {url}");
+    }
+#pragma warning restore CS0618
+
+    IEnumerator setupFiles()
+    {
+        yield return download(Application.streamingAssetsPath + "/castle.nes", Path.Combine(Application.persistentDataPath, "castle.nes"));
+        yield return download(Application.streamingAssetsPath + "/custom.pal", Path.Combine(Application.persistentDataPath, "custom.pal"));
+    }
+
+    TouchArea[] touchAreas_;
+
+    void setupUi()
+    {
+        canvas_ = FindObjectOfType<Canvas>();
+        touchAreas_ = FindObjectsOfType<TouchArea>();
+    }
+
+    void OnApplicationQuit()
+    {
+        Emulator.SaveState();
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if( pause)
+        {
+            Emulator.SaveState();
+        }
+    }
+
+    public void OnResetButtonClick()
+    {
+        Emulator.ResetMachine();
+    }
+
+}
